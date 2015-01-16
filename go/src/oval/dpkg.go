d50ed39e18ea3a3d1e94bba4c681238b66bac013
Aaron Meihm <ameihm@mozilla.com>
2015-01-16 14:01:16 -0600

    prelude to epoch/version/release OS package checks

--- go/src/oval/dpkg.go
+++ go/src/oval/dpkg.go
@@ -6,10 +6,12 @@ import (
 )
 
 type dpkgrequest struct {
-	out chan dpkgresponse
+	out  chan dpkgresponse
+	name string
 }
 
 type dpkgresponse struct {
+	pkgdata dpkgpackage
 }
 
 type dpkgdatamgr struct {
@@ -43,6 +45,21 @@ func (obj *GDPKGInfoTest) execute(od *GOvalDefinitions) bool {
 }
 
 func (state *GDPKGInfoState) evaluate(obj *GDPKGInfoObj) bool {
+	debug_prt("[dpkginfo_state] evaluate %v\n", state.ID)
+
+	dif := dpkgrequest{}
+	dif.out = make(chan dpkgresponse)
+	dif.name = obj.Name
+	dmgr.dpkg.schan <- dif
+	resp := <-dif.out
+
+	// If we get nothing back the package isn't installed
+	if resp.pkgdata.name == "" {
+		debug_prt("[dpkginfo_state] doesn't look like %v is installed\n", obj.Name)
+		return false
+	}
+	debug_prt("[dpkginfo_state] %v installed, %v\n", resp.pkgdata.name, resp.pkgdata.version)
+
 	return false
 }
 
@@ -59,15 +76,29 @@ func (d *dpkgdatamgr) prepare() {
 	d.prepared = true
 }
 
+func (d *dpkgdatamgr) build_response(req dpkgrequest) dpkgresponse {
+	ret := dpkgresponse{}
+
+	for _, x := range d.pkglist {
+		if req.name == x.name {
+			ret.pkgdata = x
+			break
+		}
+	}
+
+	return ret
+}
+
 func (d *dpkgdatamgr) run() {
 	debug_prt("Starting dpkg data manager\n")
 
 	for {
-		_, ok := <-d.schan
+		r, ok := <-d.schan
 		if ok == false {
 			debug_prt("Stopping dpkg data manager\n")
 			return
 		}
+		r.out <- d.build_response(r)
 	}
 }
 
